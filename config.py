475342b0e92a361c06fdaef2fb03cf59220a66e6
Taha Noumar <tnoumar@gmail.com>
2020-06-24 16:20:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import nltk
 #nltk config
 if nltk.download('punkt') & nltk.download('stopwords'):
     print('nltk configured successfully')
-print("Please install the modules ntlk.punkt as=ns ntlk.stopwords")
+print("Please install the modules ntlk.punkt and ntlk.stopwords")
