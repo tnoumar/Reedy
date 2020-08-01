6a26bd3cb784574eae85e6cb0ebe8f643a03edde
NOUMAR Taha <Taha.Noumar@bordeaux-inp.fr>
2020-08-01 19:26:03 +0200

    Summaize_pdf  No module named 'PyPDF2'

--- summarize_pdf.py
+++ summarize_pdf.py
@@ -2,26 +2,24 @@ import bs4 as bs #importing beautifulsoup
 import urllib.request #opens and reads urls
 import re #importing regex
 import nltk
+import PyPDF2
+import textract
 
-page = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence') #opens the wikipedia page
-article = page.read()
+arg= input("Please enter the book (.pdf) you want to summarize?")
+pdfFileObj = open('pdfs/'+str(arg), 'rb')
+print(arg)
+text = textract.process("pdfs/"+str(arg))
+#scrape text from pdf
 
-parsed_article = bs.BeautifulSoup(article,'lxml')
 
-paragraphs = parsed_article.find_all('p')
 
-article_text = ""
+article_text = text
+print(type(text))
 
-for p in paragraphs:
-    article_text += p.text # appends
 
-#formatiing the text, making all in lower case and deleting extra spaces
-article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
-article_text = re.sub(r'\s+', ' ', article_text)
-formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+formatted_article_text = re.sub(r'[^a-zA-Z]', ' ', article_text)
 formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 sentence_list = nltk.sent_tokenize(article_text)
-#print(sentence_list)
 stopwords = nltk.corpus.stopwords.words('english') #working with english stopwords
 
 word_frequencies = {}
@@ -32,7 +30,7 @@ for word in nltk.word_tokenize(formatted_article_text):
         else:
             word_frequencies[word] += 1
 
-#sorted_word_frequencies=sorted(word_frequencies.items(), key=lambda x: x[1], reverse=True)
+sorted_word_frequencies=sorted(word_frequencies.items(), key=lambda x: x[1], reverse=True)
 maximum_frequncy = max(word_frequencies.values())
 for word in word_frequencies.keys():
     word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
@@ -41,9 +39,13 @@ sentence_scores = {}
 for sent in sentence_list:
     for word in nltk.word_tokenize(sent.lower()):
         if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
+            if len(sent.split(' ')) < 40:
                 if sent not in sentence_scores.keys():
                     sentence_scores[sent] = word_frequencies[word]
                 else:
                     sentence_scores[sent] += word_frequencies[word]
-print(sentence_list)
+import heapq
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+print(summary)
--- summarize_wiki.py
+++ summarize_wiki.py
@@ -2,8 +2,21 @@ import bs4 as bs #importing beautifulsoup
 import urllib.request #opens and reads urls
 import re #importing regex
 import nltk
+import wikipedia
 
-page = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence') #opens the wikipedia page
+arg= input("Please enter the wikipedia page you want?")
+'''
+try:
+
+    url_title=wikipedia.page(arg).title
+
+except PageError(url_title):
+    print('Page not found! Try again')
+'''
+url=wikipedia.page(arg)
+print('The URL to your page is '+str(url.url))
+
+page = urllib.request.urlopen(url.url)
 article = page.read()
 
 parsed_article = bs.BeautifulSoup(article,'lxml')
@@ -21,7 +34,6 @@ article_text = re.sub(r'\s+', ' ', article_text)
 formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
 formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 sentence_list = nltk.sent_tokenize(article_text)
-#print(sentence_list)
 stopwords = nltk.corpus.stopwords.words('english') #working with english stopwords
 
 word_frequencies = {}
@@ -32,7 +44,7 @@ for word in nltk.word_tokenize(formatted_article_text):
         else:
             word_frequencies[word] += 1
 
-#sorted_word_frequencies=sorted(word_frequencies.items(), key=lambda x: x[1], reverse=True)
+sorted_word_frequencies=sorted(word_frequencies.items(), key=lambda x: x[1], reverse=True)
 maximum_frequncy = max(word_frequencies.values())
 for word in word_frequencies.keys():
     word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
@@ -41,9 +53,13 @@ sentence_scores = {}
 for sent in sentence_list:
     for word in nltk.word_tokenize(sent.lower()):
         if word in word_frequencies.keys():
-            if len(sent.split(' ')) < 30:
+            if len(sent.split(' ')) < 40:
                 if sent not in sentence_scores.keys():
                     sentence_scores[sent] = word_frequencies[word]
                 else:
                     sentence_scores[sent] += word_frequencies[word]
-print(sentence_list)
+import heapq
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+summary = ' '.join(summary_sentences)
+print(summary)
